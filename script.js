30cb779af9d679d3933d5fb7cde941eb22a36ab6
Amanda Grooms <amandafgrooms@gmail.com>
2020-02-03 12:20:57 -0600

    latest js changes

--- script.js
+++ script.js
@@ -80,40 +80,123 @@ function displayCocktailResults(responseJson) {
   getDetails(responseJson)
 }
 
+//this function will create the DOM elements to be added in the display function
+function createDomElements(newDataArr) {
+  console.log(`'createDomElements' ran`)
+	// for (let i = 0; i < newDataArray.length; i++) {
+	// 	$('.results').append(TK)
+	// }
+}
+
+//this function adds the ingredients and measurements into new arrays
+function addIngredients(newDataArr) {
+  console.log(`'addIngredients' ran`)
+	return new Promise(function (fulfill, reject) {
+    for (let i = 0; i < responseJson.meals.length; i++) {
+      for (let a = 0; a < 20; a++) {
+        if (responseJson.meals[i]["strIngredient" + a] !== null && responseJson.meals[i]["strIngredient" + a] !== "") {
+          newDataArr[i].ingredients.push(responseJson.meals[i]["strIngredient" + a])
+          newDataArr[i].measurements.push(responseJson.meals[i]["strMeasure" + a])
+        }
+      }
+    }
+		setTimeout(() => {
+			fulfill(newDataArr)
+      // console.log('final data', newDataArray)
+		}, 800)
+    console.log('final data', newDataArr)
+	})
+}
+
+// this function creates a new array with the recipe portions I'm using
+function sortObj(rawObj) {
+  console.log(`'sortObj' ran`)
+  let newDataArr = []
+	return new Promise(function (fulfill, reject) {
+		for (let i = 0; i < rawObj.meals.length; i++) {
+			newDataArr.push({
+				meal: rawObj.meals[i].strMeal,
+				instructions: rawObj.meals[i].strInstructions,
+				thumb: rawObj.meals[i].strMealThumb,
+				ingredients: [],
+				measurements: []
+			})
+		}
+		if (newDataArr.length !== rawObj.meals.length) {
+			setTimeout(() => {
+				fulfill(newDataArr)
+				console.log('setTimeout new data', newDataArr)
+			}, 300)
+		} else {
+			fulfill(newDataArr)
+      console.log('new data', newDataArr)
+		}
+	})
+}
+
 //this function will display the receipe search results in the DOM
 function displayRecipeResults(responseJson) {
   console.log(`'displayRecipeResults' ran`)
   console.log(responseJson)
   for (let i=0; i <responseJson.meals.length; i++) {
-    if (responseJson.meals.length === 0) {
-      $('.results').append(`No recipes found by that name. Try again.`)
-    } else {
-      $('.results').append(`<div id="recipe_${i}"><h3>${responseJson.meals[i].strMeal}</h3>
-      <img class="results-thumbnail" src="${responseJson.meals[i].strMealThumb}" alt="a thumbnail image of ${responseJson.meals[i].strMealThumb}"><br>
-      <button class="details-button" type="button" name="details-button">click for recipe details</button>
-      <div class="recipe-details recipe-details-hidden">
-      <p>Instructions: ${responseJson.meals[i].strInstructions}</p><br>
-      <button class="add-to-list" type="button" name="add-to-list">add to shopping list</button>
-      </div>`)
-
-
-
-
-
-      for (let a=1; a<21; a++) {
-       if (responseJson.meals[i]["strIngredient" + a] !== null && responseJson.meals[i]["strIngredient" + a] !== "") {
-         $('.results').append(`<div class="recipe-details recipe-details-hidden">
-         <ul class="ingredients-list">
-         <li><input type="checkbox" class="cb-class" name="ingredient" value="${responseJson.meals[i]["strIngredient" + a]}">${responseJson.meals[i]["strIngredient" + a]}, ${responseJson.meals[i]["strMeasure" + a]}</li>
-         </ul>
-         </div>
-         </div>`)
-      }
-    }
-  }
+	if (responseJson.meals.length !== 0) {
+		sortObj(responseJson).then(resultingObject => {
+      console.log(resultingObject)
+			// example:
+			// resultingObject = [{
+			// 		meal: obj.meals[i].strMeal,
+			// 		instructions: obj.meals[i].strInstructions,
+			// 		thumb: obj.meals[i].strMealThumb,
+			// 		ingredients: [],
+			// 		measurements: []
+			// }, {}, {}]
+			addIngredients(resultingObject).then(finalResult => {
+				createDomElements(finalResult)
+			})
+		})
+	} else {
+		// message user
+		$('.results').append(`No recipes found by that name. Try again.`)
+	}
 }
   getDetails(responseJson)
 }
+
+// function displayRecipeResults(responseJson) {
+  // console.log(`'displayRecipeResults' ran`)
+  // console.log(responseJson)
+//   sortObj(responseJson)
+//   createRecipeList(data)
+  // for (let i=0; i <responseJson.meals.length; i++) {
+  //   if (responseJson.meals.length === 0) {
+  //     $('.results').append(`No recipes found by that name. Try again.`)
+  //   } else {
+  //     $('.results').append(`<div id="recipe_${i}"><h3>${responseJson.meals[i].strMeal}</h3>
+  //     <img class="results-thumbnail" src="${responseJson.meals[i].strMealThumb}" alt="a thumbnail image of ${responseJson.meals[i].strMealThumb}"><br>
+  //     <button class="details-button" type="button" name="details-button">click for recipe details</button>
+  //     <div class="recipe-details recipe-details-hidden">
+  //     <p>Instructions: ${responseJson.meals[i].strInstructions}</p><br>
+  //     <button class="add-to-list" type="button" name="add-to-list">add to shopping list</button>
+  //     <ul class="ingredients-list">
+  //     </div>`)
+  //
+  //     let listItem = ''
+  //       for (let a=1; a<21; a++) {
+  //         if (responseJson.meals[i]["strIngredient" + a] !== null && responseJson.meals[i]["strIngredient" + a] !== "") {
+  //           listItem += '<li><input type="checkbox" class="cb-class" name="ingredient" value="${responseJson.meals[i]["strIngredient" + a]}">${responseJson.meals[i]["strIngredient" + a]}, ${responseJson.meals[i]["strMeasure" + a]}</li>'
+  //         }
+  //     }
+  //           $('.ingredients-list').append(listItem)
+  //             // $('.results').append(`<div class="recipe-details recipe-details-hidden">
+  //             // <li><input type="checkbox" class="cb-class" name="ingredient" value="${responseJson.meals[i]["strIngredient" + a]}">${responseJson.meals[i]["strIngredient" + a]}, ${responseJson.meals[i]["strMeasure" + a]}</li>
+  //             $('.results').append(`</ul>
+  //             </div>
+  //             </div>`)
+  //
+  //   }
+  // }
+//   getDetails(responseJson)
+// }
 
 // this function will fetch cocktail recipes from the CocktailDB API
 function getCocktailRecipes(searchInput, responseJson) {
